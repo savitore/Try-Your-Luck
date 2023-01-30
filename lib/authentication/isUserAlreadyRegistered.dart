9965641f19c97b209e85e17886f7d38b995f77a8
savitore <21je0482@iitism.ac.in>
2023-01-30 13:08:12 +0530

    bug fixes.

--- lib/authentication/isUserAlreadyRegistered.dart
+++ lib/authentication/isUserAlreadyRegistered.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:try_your_luck/authentication/name.dart';
 import 'package:try_your_luck/screens/home.dart';
+import 'package:http/http.dart' as http;
 
 class IsUserAlreadyRegistered extends StatefulWidget {
   const IsUserAlreadyRegistered({Key? key}) : super(key: key);
@@ -15,11 +18,49 @@ class _IsUserAlreadyRegisteredState extends State<IsUserAlreadyRegistered> {
   String? phno="";
   String name='';
   int flag=0;
-  String phoneFetched='';
+
   @override
   void initState() {
-    getData();
+    phno=FirebaseAuth.instance.currentUser?.phoneNumber;
+    fetchData();
   }
+
+  Future<void> fetchData() async{
+    String baseUrl ='https://data.mongodb-api.com/app/data-slzvn/endpoint/data/v1/action/findOne';
+    final body={
+      "dataSource":"Cluster0",
+      "database":"db",
+      "collection":"users",
+      "filter":{
+        "phone_number": phno
+      }
+    };
+    final response;
+    try{
+      response=await http.post(Uri.parse(baseUrl),
+          headers: {'Content-Type':'application/json',
+            'Accept':'application/json',
+            'Access-Control-Request-Headers':'Access-Control-Allow-Origin, Accept',
+            'api-key':'hFpu17U8fUsHjNaqLQmalJKIurolrUcYON0rkHLvTM34cT3tnpTjc5ryTPKX9W9y'},
+          body: jsonEncode(body)
+      );
+      var data = jsonDecode(response.body);
+      setState((){
+        if(response.body.length==17){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => Name()));
+        }
+        else {
+          name=data['document']['name'];
+          getData();
+          Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
+        }
+      });
+      flag=1;
+    }catch(e){
+      print(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     if(flag==1){
@@ -32,12 +73,7 @@ class _IsUserAlreadyRegisteredState extends State<IsUserAlreadyRegistered> {
   }
   void getData() async{
     var prefs = await SharedPreferences.getInstance();
-    phno =prefs.getString("phone").toString();
-    if(phno!='null'){
-      Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
-    }else{
-      Navigator.push(context, MaterialPageRoute(builder: (context) => Name()));
-    }
-    flag=1;
+    prefs.setString("phone", phno!);
+    prefs.setString("name", name);
   }
 }
