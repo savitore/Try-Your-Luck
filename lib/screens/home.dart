54f52004ba977d19bd7e3e23f2b606e1990845e1
savitore <21je0482@iitism.ac.in>
2023-04-05 17:55:46 +0530

    bug fixes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -22,6 +22,7 @@ class _HomeState extends State<Home> {
   bool prize=false, fee = false, filter = false;
   final ScrollController _scrollController = ScrollController();
   double _scrollPosition = 0;
+
   _scrollListener() {
     setState(() {
       _scrollPosition = _scrollController.position.pixels;
